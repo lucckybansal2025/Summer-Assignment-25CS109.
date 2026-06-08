c3917acdc82daf0d895cdc8af1b7641aaf640ad9
lucckybansal2025 <luccky.bansal2025@glbajajgroup.org>
2026-06-08 16:54:45 +0530

    Update Program-1.c

--- Day-1/Program-1.c
+++ Day-1/Program-1.c
@@ -1,1 +1,20 @@
-m
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n,sum=0;
+    printf("enter any number");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++) {
+    sum=sum+i;}
+    printf("%d\n",sum);
+    return 0;
+}
