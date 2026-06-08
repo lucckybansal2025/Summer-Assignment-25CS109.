2272bfbd08de13edd60d1474497153ac3d43bf8d
lucckybansal2025 <luccky.bansal2025@glbajajgroup.org>
2026-06-08 16:55:57 +0530

    Update program 4.c

--- Day-1/program 4.c
+++ Day-1/program 4.c
@@ -1,1 +1,27 @@
-m
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n,count=0;
+    printf("enter any number");
+    scanf("%d",&n);
+    if (n==0){
+    count=1;
+    }
+    else{
+    while(n!=0)
+    {
+        count=count+1;
+        n=n/10;}
+        printf("%d\n",count);
+    }
+    return count;
+}
