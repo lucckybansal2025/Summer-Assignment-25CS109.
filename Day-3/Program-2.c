ab13eb916e7527ed8fd45142468261ba30d456da
lucckybansal2025 <luccky.bansal2025@glbajajgroup.org>
2026-06-09 15:43:28 +0530

    Update Program-2.c

--- Day-3/Program-2.c
+++ Day-3/Program-2.c
@@ -1,1 +1,14 @@
-Program-2.c
+#include <stdio.h>
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    for(int i = a; i <= b; i++) {
+        int flag = 0;
+        if(i < 2) continue;
+        for(int j = 2; j <= i/2; j++) {
+            if(i % j == 0) { flag = 1; break; }
+        }
+        if(flag == 0) printf("%d ", i);
+    }
+    return 0;
+}
