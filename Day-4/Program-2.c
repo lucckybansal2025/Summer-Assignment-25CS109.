24e3109dea89012bfb94e0c2858cb2e17bd972f8
lucckybansal2025 <luccky.bansal2025@glbajajgroup.org>
2026-06-09 23:32:16 +0530

    Update Program-2.c

--- Day-4/Program-2.c
+++ Day-4/Program-2.c
@@ -1,1 +1,21 @@
-Day-1/Program-.c
+#include <stdio.h>
+int main()
+{
+    int n,a=0,b=1,i,c;
+    printf("Enter the number of terms in the Fibonacci series: ");
+    scanf("%d",&n);
+    printf("Fibonacci series: ");
+    for (i=0;i<n;i++)
+    {
+        if (i<=1)
+            c=i;
+        else
+        {
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        printf("%d ",c);
+    }
+    return 0;
+}
