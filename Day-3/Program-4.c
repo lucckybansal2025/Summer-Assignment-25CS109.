695f65ac890b81bbbfcb1a46ccb465e54bc085e1
lucckybansal2025 <luccky.bansal2025@glbajajgroup.org>
2026-06-09 15:44:17 +0530

    Update Program-4.c

--- Day-3/Program-4.c
+++ Day-3/Program-4.c
@@ -1,1 +1,15 @@
-Program-4.c
+#include <stdio.h>
+int main() {
+    int a, b, x, y;
+    scanf("%d %d", &a, &b);
+    x = a; y = b;
+    while(b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    int gcd = a;
+    int lcm = (x * y) / gcd;
+    printf("LCM = %d", lcm);
+    return 0;
+}
