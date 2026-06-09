a729a50ac3d5ea7d24d0f0a0df549b47ba357896
lucckybansal2025 <luccky.bansal2025@glbajajgroup.org>
2026-06-09 15:28:27 +0530

    Update Program-1.c

--- Day-2/Program-1.c
+++ Day-2/Program-1.c
@@ -1,1 +1,8 @@
-Day-2/Program-1.c
+#include <stdio.h>
+int main() {
+    int n, sum = 0;
+    scanf("%d", &n);
+    while(n > 0) { sum += n % 10; n /= 10; }
+    printf("%d", sum);
+    return 0;
+}
