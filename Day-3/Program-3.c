4a3371043aaec8c3e299fb896903214136087950
lucckybansal2025 <luccky.bansal2025@glbajajgroup.org>
2026-06-09 15:43:50 +0530

    Update Program-3.c

--- Day-3/Program-3.c
+++ Day-3/Program-3.c
@@ -1,1 +1,12 @@
-Program-3.c
+#include <stdio.h>
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    while(b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    printf("GCD = %d", a);
+    return 0;
+}
