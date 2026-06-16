ddc3861b82e68c99bbafaa72351dc45c062a7a91
luccky bansal <luccky.bansal2025@glbajajgroup.org>
2026-06-16 14:05:22 +0530

    Update program1.c

--- Day-11/program1.c
+++ Day-11/program1.c
@@ -1,1 +1,14 @@
-program1.c
+#include <stdio.h>
+int sum(int a, int b)
+{
+    return a + b;
+
+}
+int main(){
+    int n,m;
+    printf("Enter the two numbers:");
+    scanf("%d%d",&n,&m);
+    int s=sum(n,m);
+    printf("%d + %d = %d",n,m,s);
+    return 0;
+}
